ef1c0f3e4a0622863011adcbb4f5d6df0f38424d
Just Emandleni <s216673380@mandela.ac.za>
2021-07-08 21:16:25 +0200

    Add project files.

---
+++ WindowsFormsApp/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace WindowsFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelUsername = new System.Windows.Forms.Label();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.labelAccessLevel = new System.Windows.Forms.Label();
+            this.textBoxFirstName = new System.Windows.Forms.TextBox();
+            this.labelFirstName = new System.Windows.Forms.Label();
+            this.textBoxLastName = new System.Windows.Forms.TextBox();
+            this.labelLastName = new System.Windows.Forms.Label();
+            this.textBoxConfirmPassword = new System.Windows.Forms.TextBox();
+            this.labelConfirmPassword = new System.Windows.Forms.Label();
+            this.buttonAddUser = new System.Windows.Forms.Button();
+            this.comboBoxAccessLevel = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // labelUsername
+            // 
+            this.labelUsername.AutoSize = true;
+            this.labelUsername.Location = new System.Drawing.Point(34, 45);
+            this.labelUsername.Name = "labelUsername";
+            this.labelUsername.Size = new System.Drawing.Size(65, 15);
+            this.labelUsername.TabIndex = 0;
+            this.labelUsername.Text = "Username";
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.Location = new System.Drawing.Point(37, 63);
+            this.textBoxUsername.MaxLength = 50;
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(201, 20);
+            this.textBoxUsername.TabIndex = 1;
+            this.textBoxUsername.WordWrap = false;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(37, 120);
+            this.textBoxPassword.MaxLength = 50;
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.PasswordChar = '*';
+            this.textBoxPassword.Size = new System.Drawing.Size(201, 20);
+            this.textBoxPassword.TabIndex = 2;
+            this.textBoxPassword.WordWrap = false;
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Location = new System.Drawing.Point(34, 102);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(61, 15);
+            this.labelPassword.TabIndex = 2;
+            this.labelPassword.Text = "Password";
+            // 
+            // labelAccessLevel
+            // 
+            this.labelAccessLevel.AutoSize = true;
+            this.labelAccessLevel.Location = new System.Drawing.Point(34, 205);
+            this.labelAccessLevel.Name = "labelAccessLevel";
+            this.labelAccessLevel.Size = new System.Drawing.Size(77, 15);
+            this.labelAccessLevel.TabIndex = 4;
+            this.labelAccessLevel.Text = "Access Level";
+            // 
+            // textBoxFirstName
+            // 
+            this.textBoxFirstName.Location = new System.Drawing.Point(37, 280);
+            this.textBoxFirstName.MaxLength = 50;
+            this.textBoxFirstName.Name = "textBoxFirstName";
+            this.textBoxFirstName.Size = new System.Drawing.Size(201, 20);
+            this.textBoxFirstName.TabIndex = 5;
+            this.textBoxFirstName.WordWrap = false;
+            // 
+            // labelFirstName
+            // 
+            this.labelFirstName.AutoSize = true;
+            this.labelFirstName.Location = new System.Drawing.Point(34, 262);
+            this.labelFirstName.Name = "labelFirstName";
+            this.labelFirstName.Size = new System.Drawing.Size(67, 15);
+            this.labelFirstName.TabIndex = 6;
+            this.labelFirstName.Text = "First Name";
+            // 
+            // textBoxLastName
+            // 
+            this.textBoxLastName.Location = new System.Drawing.Point(37, 336);
+            this.textBoxLastName.MaxLength = 50;
+            this.textBoxLastName.Name = "textBoxLastName";
+            this.textBoxLastName.Size = new System.Drawing.Size(201, 20);
+            this.textBoxLastName.TabIndex = 6;
+            this.textBoxLastName.WordWrap = false;
+            // 
+            // labelLastName
+            // 
+            this.labelLastName.AutoSize = true;
+            this.labelLastName.Location = new System.Drawing.Point(34, 318);
+            this.labelLastName.Name = "labelLastName";
+            this.labelLastName.Size = new System.Drawing.Size(67, 15);
+            this.labelLastName.TabIndex = 8;
+            this.labelLastName.Text = "Last Name";
+            // 
+            // textBoxConfirmPassword
+            // 
+            this.textBoxConfirmPassword.Location = new System.Drawing.Point(37, 172);
+            this.textBoxConfirmPassword.MaxLength = 50;
+            this.textBoxConfirmPassword.Name = "textBoxConfirmPassword";
+            this.textBoxConfirmPassword.PasswordChar = '*';
+            this.textBoxConfirmPassword.Size = new System.Drawing.Size(201, 20);
+            this.textBoxConfirmPassword.TabIndex = 3;
+            this.textBoxConfirmPassword.WordWrap = false;
+            // 
+            // labelConfirmPassword
+            // 
+            this.labelConfirmPassword.AutoSize = true;
+            this.labelConfirmPassword.Location = new System.Drawing.Point(34, 154);
+            this.labelConfirmPassword.Name = "labelConfirmPassword";
+            this.labelConfirmPassword.Size = new System.Drawing.Size(107, 15);
+            this.labelConfirmPassword.TabIndex = 10;
+            this.labelConfirmPassword.Text = "Confirm Password";
+            // 
+            // buttonAddUser
+            // 
+            this.buttonAddUser.Location = new System.Drawing.Point(86, 391);
+            this.buttonAddUser.Name = "buttonAddUser";
+            this.buttonAddUser.Size = new System.Drawing.Size(99, 23);
+            this.buttonAddUser.TabIndex = 11;
+            this.buttonAddUser.Text = "Create User";
+            this.buttonAddUser.UseVisualStyleBackColor = true;
+            this.buttonAddUser.Click += new System.EventHandler(this.buttonAddUser_Click);
+            // 
+            // comboBoxAccessLevel
+            // 
+            this.comboBoxAccessLevel.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxAccessLevel.FormattingEnabled = true;
+            this.comboBoxAccessLevel.Location = new System.Drawing.Point(37, 228);
+            this.comboBoxAccessLevel.Name = "comboBoxAccessLevel";
+            this.comboBoxAccessLevel.Size = new System.Drawing.Size(201, 21);
+            this.comboBoxAccessLevel.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 505);
+            this.Controls.Add(this.comboBoxAccessLevel);
+            this.Controls.Add(this.buttonAddUser);
+            this.Controls.Add(this.textBoxConfirmPassword);
+            this.Controls.Add(this.labelConfirmPassword);
+            this.Controls.Add(this.textBoxLastName);
+            this.Controls.Add(this.labelLastName);
+            this.Controls.Add(this.textBoxFirstName);
+            this.Controls.Add(this.labelFirstName);
+            this.Controls.Add(this.labelAccessLevel);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.textBoxUsername);
+            this.Controls.Add(this.labelUsername);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelUsername;
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.Label labelAccessLevel;
+        private System.Windows.Forms.TextBox textBoxFirstName;
+        private System.Windows.Forms.Label labelFirstName;
+        private System.Windows.Forms.TextBox textBoxLastName;
+        private System.Windows.Forms.Label labelLastName;
+        private System.Windows.Forms.TextBox textBoxConfirmPassword;
+        private System.Windows.Forms.Label labelConfirmPassword;
+        private System.Windows.Forms.Button buttonAddUser;
+        private System.Windows.Forms.ComboBox comboBoxAccessLevel;
+    }
+}
+
---
+++ WindowsFormsApp/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection CONNECTION;
+        string CONNECTION_STRING = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Admin\Documents\TEAM_12_ULTRON_3_FORAGE_DATABASE.mdf;Integrated Security=True;Connect Timeout=30";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            populateComboBox();
+        }
+
+        private void populateComboBox()
+        {
+            SqlConnection conn = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=AutoEDITest;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+            string query = "SELECT Id, Name FROM AccessLevel;";
+            SqlDataAdapter da = new SqlDataAdapter(query, conn);
+            conn.Open();
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            DataRow row = dt.NewRow();
+            row[0] = 0;
+            dt.Rows.InsertAt(row, 0);
+
+            comboBoxAccessLevel.DataSource = dt;
+            comboBoxAccessLevel.DisplayMember = "Name";
+            comboBoxAccessLevel.ValueMember = "Id";
+
+        }
+
+        private bool isMatching(string a, string b)
+        {
+            if (a.Equals(b))
+                return true;
+            else return false;
+        }
+
+        private bool isExisting(string str1, SqlConnection connection)
+        {
+            string SQL_SELECT_STMNT = "SELECT * FROM Student WHERE studentUsername = '" + str1 + "'";
+
+            SqlDataAdapter DATA_ADAPTER = new SqlDataAdapter(SQL_SELECT_STMNT, connection);
+            DataTable DATA_TABLE = new DataTable();
+            DATA_ADAPTER.Fill(DATA_TABLE);
+
+            if (DATA_TABLE.Rows.Count > 0)
+                return true;
+            else return false;
+        }
+
+
+        private void buttonAddUser_Click(object sender, EventArgs e)
+        {
+            if (!isMatching(textBoxPassword.Text, textBoxConfirmPassword.Text))
+            {
+                MessageBox.Show("Passwords do not match", "", MessageBoxButtons.RetryCancel, MessageBoxIcon.Warning);
+                textBoxConfirmPassword.Clear();
+                return;
+            }
+            if (!isExisting(textBoxUsername.Text, CONNECTION))
+            {
+                SqlCommand command = new SqlCommand(@"INSERT INTO User (Username, Password, AccessLevelId, FirstName, LastName) 
+                  VALUES ('" + textBoxUsername.Text + "', " +
+                         "'" + textBoxPassword.Text + "', " +
+                         "'" + comboBoxAccessLevel.Text + "', " +
+                         "'" + textBoxFirstName.Text + "', " +
+                         "'" + textBoxLastName.Text + 
+                         "')", CONNECTION);
+                command.ExecuteNonQuery();
+                CONNECTION.Close();
+                MessageBox.Show("Successfully registered " + textBoxFirstName.Text + "!", "Complete", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("We already know you here " + textBoxFirstName.Text + "! Rather log in.", "Account already exists", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
